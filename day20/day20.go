46a559e775f5abaac6af88462987b163b20eb7db
Gerald Oster <gerald.oster@loria.fr>
2022-12-21 00:41:11 +0100

    [day20] solution for Part 1

---
+++ day20/day20.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func ParseInput() (numbers []int) {
+	numbers = make([]int, 0)
+
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		n, _ := strconv.Atoi(scanner.Text())
+		numbers = append(numbers, n)
+	}
+
+	return numbers
+}
+
+func GetValues(numbers []int, indexes []int) (values []int) {
+	values = make([]int, len(numbers))
+	for j := 0; j < len(numbers); j++ {
+		values[indexes[j]] = numbers[j]
+	}
+	return values
+}
+
+func PrintValues(numbers []int, indexes []int) {
+	var values []int = GetValues(numbers, indexes)
+
+	fmt.Print("[ ")
+	for j := 0; j < len(numbers); j++ {
+		fmt.Print(values[j], " ")
+	}
+	fmt.Println("]")
+}
+
+func Part1(numbers []int) int {
+	size := len(numbers)
+	var indexes []int = make([]int, size)
+
+	for i := 0; i < size; i++ {
+		indexes[i] = i
+	}
+
+	for index, shift := range numbers {
+		newIndex := (indexes[index] + shift + 2*(size-1)) % (size - 1)
+
+		if newIndex == 0 {
+			newIndex = size - 1
+		}
+
+		if shift == 0 {
+			continue
+		}
+
+		oldIndex := indexes[index]
+		indexes[index] = newIndex
+
+		if oldIndex > newIndex {
+			for otherIndex := 0; otherIndex < size; otherIndex++ {
+				if otherIndex == index {
+					continue
+				}
+				if indexes[otherIndex] >= newIndex && indexes[otherIndex] < oldIndex {
+					indexes[otherIndex] = (indexes[otherIndex] + 1)
+				}
+			}
+		} else {
+			for otherIndex := 0; otherIndex < size; otherIndex++ {
+				if otherIndex == index {
+					continue
+				}
+				if indexes[otherIndex] >= oldIndex && indexes[otherIndex] <= newIndex {
+					indexes[otherIndex] = (indexes[otherIndex] - 1)
+				}
+			}
+		}
+
+		// PrintValues(numbers, indexes)
+	}
+
+	// fmt.Println(indexes)
+
+	PrintValues(numbers, indexes)
+	values := GetValues(numbers, indexes)
+	indexOfZero := -1
+	for i := 0; i < size; i++ {
+		if values[i] == 0 {
+			indexOfZero = i
+			break
+		}
+	}
+
+	a := values[(indexOfZero+1000)%size] // should be 4
+	fmt.Println(a)
+	b := values[(indexOfZero+2000)%size] // should be -3
+	fmt.Println(b)
+	c := values[(indexOfZero+3000)%size] // should be 2
+	fmt.Println(c)
+
+	return a + b + c // should be 3 for sample
+}
+
+func Part2() int {
+	return 0
+}
+
+func Solve() (int, int) {
+	numbers := ParseInput()
+
+	part1 := Part1(numbers) // 3, 27726
+	part2 := 0
+	return part1, part2
+}
