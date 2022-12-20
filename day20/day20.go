02ef8e0f1db149fdde2b1ef4cdefa5c7f3d26755
Gerald Oster <gerald.oster@loria.fr>
2022-12-21 00:51:08 +0100

    [day20] Solution for Part 2

--- day20/day20.go
+++ day20/day20.go
@@ -90,7 +90,82 @@ func Part1(numbers []int) int {
 
 	// fmt.Println(indexes)
 
-	PrintValues(numbers, indexes)
+	//PrintValues(numbers, indexes)
+	values := GetValues(numbers, indexes)
+	indexOfZero := -1
+	for i := 0; i < size; i++ {
+		if values[i] == 0 {
+			indexOfZero = i
+			break
+		}
+	}
+
+	a := values[(indexOfZero+1000)%size] // should be 4
+	// fmt.Println(a)
+	b := values[(indexOfZero+2000)%size] // should be -3
+	// fmt.Println(b)
+	c := values[(indexOfZero+3000)%size] // should be 2
+	// fmt.Println(c)
+
+	return a + b + c // should be 3 for sample
+}
+
+func Part2(numbers []int) int {
+	encryptionKey := 811589153
+
+	size := len(numbers)
+	for i := 0; i < size; i++ {
+		numbers[i] *= encryptionKey
+	}
+	var indexes []int = make([]int, size)
+
+	for i := 0; i < size; i++ {
+		indexes[i] = i
+	}
+
+	for k := 0; k < 10; k++ {
+
+		for index, shift := range numbers {
+			newIndex := (indexes[index] + shift + 10*encryptionKey*(size-1)) % (size - 1)
+
+			if newIndex == 0 {
+				newIndex = size - 1
+			}
+
+			if shift == 0 {
+				continue
+			}
+
+			oldIndex := indexes[index]
+			indexes[index] = newIndex
+
+			if oldIndex > newIndex {
+				for otherIndex := 0; otherIndex < size; otherIndex++ {
+					if otherIndex == index {
+						continue
+					}
+					if indexes[otherIndex] >= newIndex && indexes[otherIndex] < oldIndex {
+						indexes[otherIndex] = (indexes[otherIndex] + 1)
+					}
+				}
+			} else {
+				for otherIndex := 0; otherIndex < size; otherIndex++ {
+					if otherIndex == index {
+						continue
+					}
+					if indexes[otherIndex] >= oldIndex && indexes[otherIndex] <= newIndex {
+						indexes[otherIndex] = (indexes[otherIndex] - 1 + 2*(size-1)) % (size - 1)
+					}
+				}
+			}
+
+			// PrintValues(numbers, indexes)
+		}
+	}
+
+	// fmt.Println(indexes)
+
+	//PrintValues(numbers, indexes)
 	values := GetValues(numbers, indexes)
 	indexOfZero := -1
 	for i := 0; i < size; i++ {
@@ -110,14 +185,10 @@ func Part1(numbers []int) int {
 	return a + b + c // should be 3 for sample
 }
 
-func Part2() int {
-	return 0
-}
-
 func Solve() (int, int) {
 	numbers := ParseInput()
 
 	part1 := Part1(numbers) // 3, 27726
-	part2 := 0
+	part2 := Part2(numbers)
 	return part1, part2
 }
