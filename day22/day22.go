695db9383505f7b717af4a55814adc99fe491ad6
Gerald Oster <gerald.oster@loria.fr>
2022-12-22 23:56:21 +0100

    [day22] Solution of Part 1

--- day22/day22.go
+++ day22/day22.go
@@ -4,55 +4,96 @@ import (
 	"bufio"
 	_ "embed"
 	"fmt"
+	"math"
 	"strings"
 )
 
-//go:embed sample.txt
+//go:embed input.txt
 var input string
 
-type Direction struct {
-	yDirection int
-	xDirection int
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
-var NORTH = Direction{1, 0}
-var EAST = Direction{0, 1}
-var SOUTH = Direction{-1, 0}
-var WEST = Direction{0, -1}
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
-var HEADINGS [4]Direction = [4]Direction{NORTH, EAST, SOUTH, WEST}
+type Pos struct {
+	x int
+	y int
+}
+
+type Interval struct {
+	min int
+	max int
+}
+
+type Direction Pos // just a nice alias
+
+var NORTH = Direction{0, -1}
+var EAST = Direction{1, 0}
+var SOUTH = Direction{0, 1}
+var WEST = Direction{-1, 0}
+
+var HEADINGS [4]Direction = [4]Direction{EAST, SOUTH, WEST, NORTH}
+var HEADINGS_SIZE = len(HEADINGS)
+
+var FACING_CHAR [4]byte = [4]byte{'>', 'v', '<', '^'}
+
+func GetFacingAsChar(facing int) byte {
+	return FACING_CHAR[facing]
+}
 
 type Move struct {
 	length   int
 	rotation int
 }
 
-func ParseInput() (data [][]byte, instructions []Move) {
+func ParseInput() (data [][]byte, rows map[int]Interval, columns map[int]Interval, moves []Move) {
+	rows = make(map[int]Interval)
+	columns = make(map[int]Interval)
 	data = make([][]byte, 0)
-	instructions = make([]Move, 0)
+	moves = make([]Move, 0)
 
 	scanner := bufio.NewScanner(strings.NewReader(input))
 	scanner.Split(bufio.ScanLines)
 
+	rowIdx := 0
 	for scanner.Scan(); len(scanner.Text()) > 1; scanner.Scan() {
 		line := strings.TrimRight(scanner.Text(), "\n")
-		var dataLine []byte = make([]byte, len(line))
 
-		for idx := 0; idx < len(line); idx++ {
-			char := line[idx]
-			switch char {
-			case ' ':
-				dataLine[idx] = 0
+		var rowInterval Interval = Interval{min: math.MaxInt, max: math.MinInt}
 
-			case '#', '.':
-				dataLine[idx] = char
+		var dataLine []byte = make([]byte, 0)
+		var columnIdx = 0
+		for columnIdx = 0; columnIdx < len(line); columnIdx++ {
+			columnInterval, ok := columns[columnIdx]
+			if !ok {
+				columnInterval = Interval{min: math.MaxInt, max: math.MinInt}
+			}
 
-			default:
-				panic("invalid character in puzzle map description" + fmt.Sprintf("[%c]", char))
+			char := line[columnIdx]
+			if char == '.' || char == '#' {
+				dataLine = append(dataLine, char)
+				rowInterval.min = min(columnIdx, rowInterval.min)
+				rowInterval.max = max(columnIdx, rowInterval.min)
+				columnInterval.min = min(rowIdx, columnInterval.min)
+				columnInterval.max = max(rowIdx, columnInterval.min)
+				columns[columnIdx] = columnInterval
 			}
 		}
 		data = append(data, dataLine)
+		rows[rowIdx] = rowInterval
+		rowIdx++
 	}
+
 	scanner.Scan()
 	lastLine := scanner.Text()
 
@@ -65,7 +106,7 @@ func ParseInput() (data [][]byte, instructions []Move) {
 			value = value*10 + int(char-'0')
 			continue
 		} else {
-			instructions = append(instructions, Move{length: value, rotation: 0})
+			moves = append(moves, Move{length: value, rotation: 0})
 		}
 
 		switch char {
@@ -78,33 +119,93 @@ func ParseInput() (data [][]byte, instructions []Move) {
 		default:
 			panic("invalid rotation instruction in puzzle description")
 		}
-		instructions = append(instructions, move)
+		moves = append(moves, move)
 	}
 
 	if value != 0 {
-		instructions = append(instructions, Move{length: value, rotation: 0})
+		moves = append(moves, Move{length: value, rotation: 0})
 	}
-	return data, instructions
+	return data, rows, columns, moves
 }
 
-func PrintMap(data [][]byte) {
-	for y := 0; y < len(data); y++ {
-		for x := 0; x < len(data[y]); x++ {
-			switch data[y][x] {
-			case 0:
-				fmt.Print(" ")
-			case '#':
-				fmt.Print("#")
-			case '.':
-				fmt.Print(".")
-			}
+func PrintMap(data [][]byte, rows map[int]Interval, columns map[int]Interval) {
+	// need to be rewritten since we change the datastructures in the meantime...
+	for rowIndex := 0; rowIndex < len(data); rowIndex++ {
+		for columnIndex := 0; columnIndex < rows[rowIndex].min; columnIndex++ {
+			fmt.Print(" ")
+		}
+		for columnIndex := rows[rowIndex].min; columnIndex < rows[rowIndex].max; columnIndex++ {
+			fmt.Printf("%c", data[rowIndex][columnIndex-rows[rowIndex].min])
 		}
 		fmt.Println()
 	}
 }
 
-func Part1() int {
-	return 0
+func Part1(data [][]byte, rows map[int]Interval, columns map[int]Interval, moves []Move) int {
+	var current Pos = Pos{rows[0].min, columns[rows[0].min].min} // top-left most open tile
+	facing := 0                                                  // facing to Right
+
+	// fmt.Println("current.x:", current.x, "current.y:", current.y)
+
+	if data[current.y][current.x-rows[current.y].min] != '.' {
+		panic("wrong starting coord:" + fmt.Sprintf("%v", current))
+	}
+
+	// data[current.y][current.x-rows[current.y].min] = 'S'
+	// fmt.Printf("%c at (%d,%d)\n", GetFacingAsChar(facing), current.x, current.y)
+	// PrintMap(data, rows, columns)
+
+	for _, move := range moves {
+
+		if move.length == 0 {
+			// only rotation
+			facing = (facing + move.rotation + HEADINGS_SIZE) % HEADINGS_SIZE
+			continue
+		}
+
+		var heading Direction = HEADINGS[facing]
+
+		var next Pos = current
+		for step := 0; step < move.length; step++ {
+			heading = HEADINGS[facing]
+
+			next.x = (current.x + heading.x)
+			if next.x > rows[next.y].max {
+				next.x = rows[next.y].min
+			} else if next.x < rows[next.y].min {
+				next.x = rows[next.y].max
+			}
+
+			next.y = (current.y + heading.y)
+			if next.y > columns[next.x].max {
+				next.y = columns[next.x].min
+			} else if next.y < columns[next.x].min {
+				next.y = columns[next.x].max
+			}
+
+			// fmt.Printf("%c virtually at (%d,%d)\n", GetFacingAsChar(facing), next.x, next.y)
+			if data[next.y][next.x-rows[next.y].min] == '.' {
+				// mark the last position
+				// data[current.y][next.x-rows[next.y].min] = GetFacingAsChar(facing)
+				current = next
+			} else {
+				// can't move
+				break
+			}
+		}
+		// fmt.Println()
+		// PrintMap(data, rows, columns)
+		// os.Stdin.Read(make([]byte, 1))
+	}
+
+	// fmt.Printf("x: %d, y: %d, facing: %d\n", current.x, current.y, facing)
+	// expected values for sample
+	// current.x = 7
+	// current.y = 5
+	// facing = 0
+	// fmt.Printf("x: %d, y: %d, facing: %d  (expectations)\n", current.x, current.y, facing)
+
+	return 1000*(current.y+1) + 4*(current.x+1) + facing
 }
 
 func Part2() int {
@@ -113,17 +214,19 @@ func Part2() int {
 
 func Solve() (int, int) {
 	var data [][]byte
-	var instructions []Move
+	var moves []Move
+	var rows map[int]Interval
+	var columns map[int]Interval
+	data, rows, columns, moves = ParseInput()
 
-	data, instructions = ParseInput()
+	// fmt.Println("rows intervals: ", rows)
+	// fmt.Println("columns intervals: ", columns)
+	// fmt.Println()
+	// PrintMap(data, rows, columns)
+	// fmt.Println()
+	// fmt.Println(moves)
 
-	PrintMap(data)
-	fmt.Println()
-	fmt.Println(instructions)
-
-	data[0] = nil
-	instructions[0] = Move{}
-	part1 := 0
+	part1 := Part1(data, rows, columns, moves)
 	part2 := 0
 	return part1, part2
 }
