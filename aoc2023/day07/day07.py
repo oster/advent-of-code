a257a69c7a41ee96b6d9a0bd09d2887b2c5f5ff3
Gerald Oster <gerald.oster@loria.fr>
2023-12-07 13:45:37 +0100

    [day7] cleaning up

--- aoc2023/day07/day07.py
+++ aoc2023/day07/day07.py
@@ -1,102 +1,109 @@
-from typing import Generator
+from typing import Generator, Callable
+from enum import Enum
 
 def read_input(filename : str) -> Generator[str, None, None]:
     with open(filename, 'r') as input_file:
         for line in input_file.readlines():
             yield line.rstrip()
 
-FIVE_OF_A_KIND = 7
-FOUR_OF_A_KIND = 6
-FULL_HOUSE = 5
-THREE_OF_A_KIND = 4
-TWO_PAIRS = 3
-ONE_PAIR = 2
-HIGH_CARD = 1
 
-def hand_type(cards, card_symbols) -> int:
-    occurrences = [0] * len(card_symbols)
-    for card in cards:
-        occurrences[card-1] += 1
-    occurrences = sorted(occurrences, reverse=True)
-    max = occurrences[0]
-    max2 = occurrences[1]
+class HandType(Enum):
+    FIVE_OF_A_KIND = 7
+    FOUR_OF_A_KIND = 6
+    FULL_HOUSE = 5
+    THREE_OF_A_KIND = 4
+    TWO_PAIRS = 3
+    ONE_PAIR = 2
+    HIGH_CARD = 1
+
+
+def compute_hand_type(cards : str, card_symbols : list[str]) -> HandType:
+    occurrences = sorted([ cards.count(card_symbols[idx]) for idx in range(len(card_symbols))], reverse=True)
+
+    max, max2 = occurrences[0], occurrences[1]
 
     match max:
-        case 5: return FIVE_OF_A_KIND
-        case 4: return FOUR_OF_A_KIND
+        case 5: 
+            return HandType.FIVE_OF_A_KIND
+        case 4: 
+            return HandType.FOUR_OF_A_KIND
         case 3:
             match max2:
-                case 2: return FULL_HOUSE
-                case _: return THREE_OF_A_KIND
+                case 2: 
+                    return HandType.FULL_HOUSE
+                case _: 
+                    return HandType.THREE_OF_A_KIND
         case 2: 
             match max2:
-                case 2: return TWO_PAIRS
-                case _: return ONE_PAIR
-        case _: return HIGH_CARD
+                case 2: 
+                    return HandType.TWO_PAIRS
+                case _: 
+                    return HandType.ONE_PAIR
+        case _: 
+            return HandType.HIGH_CARD
 
 
-def hand_type_part2(cards, card_symbols) -> int:
-    cards = cards[:]
+def compute_hand_type_with_joker(cards : str, card_symbols : list[str]) -> HandType:
+    joker_count = cards.count('J') # joker is the last card
+    cards_without_joker = ''.join(filter(lambda card: card != 'J', cards))
+    hand_type = compute_hand_type(cards_without_joker, card_symbols)
 
-    joker_value = 1
-    joker_count = cards.count(joker_value) # joker is the last card
+    match joker_count:
+        case 5: return HandType.FIVE_OF_A_KIND
+        case 4: return HandType.FIVE_OF_A_KIND
+        case 3:
+            match hand_type:
+                case HandType.ONE_PAIR: 
+                    return HandType.FIVE_OF_A_KIND
+                case _: 
+                    return HandType.FOUR_OF_A_KIND
+        case 2:
+            match hand_type:
+                case HandType.THREE_OF_A_KIND: 
+                    return HandType.FIVE_OF_A_KIND
+                case HandType.ONE_PAIR: 
+                    return HandType.FOUR_OF_A_KIND
+                case _: 
+                    return HandType.THREE_OF_A_KIND
+        case 1:
+            match hand_type:
+                case HandType.FOUR_OF_A_KIND: 
+                    return HandType.FIVE_OF_A_KIND
+                case HandType.THREE_OF_A_KIND: 
+                    return HandType.FOUR_OF_A_KIND
+                case HandType.TWO_PAIRS: 
+                    return HandType.FULL_HOUSE
+                case HandType.ONE_PAIR: 
+                    return HandType.THREE_OF_A_KIND
+                case _: 
+                    return HandType.ONE_PAIR
+        case 0:
+                return compute_hand_type(cards, card_symbols)
+        case _: 
+            return HandType.HIGH_CARD
+        
 
-    cards_without_joker = list(filter(lambda card: joker_value != card, cards))
-    type = hand_type(cards_without_joker, card_symbols)
-
-    if joker_count == 0:
-        return hand_type(cards, card_symbols)
-
-    # so there is at least one joker
-    if joker_count == 5:
-        return FIVE_OF_A_KIND
-    if joker_count == 4: 
-        return FIVE_OF_A_KIND
-    if joker_count == 3:
-        if type == ONE_PAIR:
-            return FIVE_OF_A_KIND
-        return FOUR_OF_A_KIND
-    if joker_count == 2:
-        if type == THREE_OF_A_KIND:
-            return FIVE_OF_A_KIND
-        if type == ONE_PAIR:
-            return FOUR_OF_A_KIND
-        return THREE_OF_A_KIND
-    # if joker_count == 1:
-    if type == FOUR_OF_A_KIND:
-        return FIVE_OF_A_KIND
-    if type == THREE_OF_A_KIND:
-        return FOUR_OF_A_KIND
-    if type == TWO_PAIRS:
-        return FULL_HOUSE
-    if type == ONE_PAIR:
-        return THREE_OF_A_KIND            
-    return ONE_PAIR
+def encode_cards(cards : str, card_symbols) -> int:
+    def value(card : str) -> int:
+        return card_symbols.index(card)
+    return  (((value(cards[0]) << 4 | value(cards[1])) << 4 | value(cards[2])) << 4 | value(cards[3])) << 4 | value(cards[4])
 
 
-def encode_cards(cards):
-    return  (((cards[0] << 4 | cards[1]) << 4 | cards[2]) << 4 | cards[3]) << 4 | cards[4]
-
-
-def parse_input(filename : str, card_symbols, hand_typer) -> list:
+def parse_input(filename : str, card_symbols, hand_typer : Callable[[str, list[str]], HandType]) -> list[tuple[str, int, int]]:
     inputs = read_input(filename)
 
     hands = []
     for line in inputs:
         cards, bids = line.split()
-        # cards_str = str(cards)
-        cards = [ 1 + card_symbols.index(card) for card in cards ]
-
         hand_type = hand_typer(cards, card_symbols)
-        value = hand_type << 20 | encode_cards(cards)
-
+        value = hand_type.value << 20 | encode_cards(cards, card_symbols)
         bids = int(bids)
         hands.append((cards, value, bids))
 
     return hands
 
 
-def compute_winnings(hands):
+def compute_winnings(hands : list[tuple[str, int, int]]) -> int:
     res = 0
     for rank, hand in enumerate(sorted(hands, key=lambda hand: hand[1])):
         res += hand[2] * (rank + 1)
@@ -104,11 +111,12 @@ def compute_winnings(hands):
 
 
 def part1(filename : str) -> int:
-    hands = parse_input(filename, ['2','3','4','5','6','7','8','9','T','J','Q','K','A'], hand_type)
+    hands = parse_input(filename, ['2','3','4','5','6','7','8','9','T','J','Q','K','A'], compute_hand_type)
     return compute_winnings(hands)
 
+
 def part2(filename : str) -> int:
-    hands = parse_input(filename, ['J','2','3','4','5','6','7','8','9','T','Q','K','A'], hand_type_part2)
+    hands = parse_input(filename, ['J','2','3','4','5','6','7','8','9','T','Q','K','A'], compute_hand_type_with_joker)
     return compute_winnings(hands)
 
 
