571dd9b8a4184289a772eb87a5ae728e10075258
Gerald Oster <gerald.oster@loria.fr>
2023-12-10 13:45:23 +0100

    [day10] cleanup (a bit)

--- aoc2023/day10/day10.py
+++ aoc2023/day10/day10.py
@@ -22,33 +22,37 @@ def parse_input(filename : str) -> tuple[tuple[int, int], list[list[str]]]:
         row = list(lines)
         if 'S' in row:
             start = (row_index, row.index('S'))
-        grid.append(list(lines))
-
+        
+        grid.append(row)
 
     assert start != (-1, -1)
 
     return (start, grid)
 
 
-
 def print_grid(grid : list[list[str]]) -> None:
     for row in grid:
-        print(''.join(row))
+        print(''.join(row).replace('│', '║').replace('─','═').replace('┌','╔').replace('┐','╗').replace('└','╚').replace('┘','╝').replace('.','█'))
     print()
+
 
 def print_numbers(grid : list[list[int]]) -> None:
     for row in grid:
-        for colum in row:
-            if colum == -1:
-                print('SS', end='')
-            elif colum == 0:
-                print('..', end='')
+        for column in row:
+            if column == -1:
+                print('S', end='')
+                # print('SS', end='')
+            elif column == 0:
+                print('.', end='')
+                # print('..', end='')
             else:
-                print(f"{colum:02d}", end='')
+                print(f"{column}", end='')
+                # print(f"{colum:02d}", end='')
         print()
     print()
 
-def find_max(numbers : list[list[int]]) -> int:
+
+def find_max_in_grid(numbers : list[list[int]]) -> int:
     max = 0
     for row in numbers:
         for colum in row:
@@ -57,71 +61,64 @@ def find_max(numbers : list[list[int]]) -> int:
     return max  
 
 
-def flood(y, x, step, h, w, g, n):
+def flood_pipe(y, x, step, h, w, g, n):
     if y < 0 or y >= h or x < 0 or x >= w:
         return
     if n[y][x] != 0 and n[y][x] <= step:
         return
     n[y][x] = step
 
-    if g[y][x] == '│':
-        flood(y+1, x, step+1, h, w, g, n)
-        flood(y-1, x, step+1, h, w, g, n)
-    elif g[y][x] == '─':
-        flood(y, x+1, step+1, h, w, g, n)
-        flood(y, x-1, step+1, h, w, g, n)
-    elif g[y][x] == '┌':
-        flood(y+1, x, step+1, h, w, g, n)
-        flood(y, x+1, step+1, h, w, g, n)
-    elif g[y][x] == '└':
-        flood(y, x+1, step+1, h, w, g, n)
-        flood(y-1, x, step+1, h, w, g, n)
-    elif g[y][x] == '┐':
-        flood(y, x-1, step+1, h, w, g, n)
-        flood(y+1, x, step+1, h, w, g, n)
-    elif g[y][x] == '┘':
-        flood(y-1, x, step+1, h, w, g, n)
-        flood(y, x-1, step+1, h, w, g, n)
+    match g[y][x]:
+        case '│':
+            flood_pipe(y+1, x, step+1, h, w, g, n)
+            flood_pipe(y-1, x, step+1, h, w, g, n)
+        case '─':
+            flood_pipe(y, x+1, step+1, h, w, g, n)
+            flood_pipe(y, x-1, step+1, h, w, g, n)
+        case '┌':
+            flood_pipe(y+1, x, step+1, h, w, g, n)
+            flood_pipe(y, x+1, step+1, h, w, g, n)
+        case '┐':
+            flood_pipe(y, x-1, step+1, h, w, g, n)
+            flood_pipe(y+1, x, step+1, h, w, g, n)
+        case '┘':
+            flood_pipe(y-1, x, step+1, h, w, g, n)
+            flood_pipe(y, x-1, step+1, h, w, g, n)
+        case '└':
+            flood_pipe(y, x+1, step+1, h, w, g, n)
+            flood_pipe(y-1, x, step+1, h, w, g, n)
+        case _:
+            return
 
 
-
-
-def compute_main_loop(start, grid : list[list[str]]) -> list[list[int]]:
+def compute_main_pipe_loop(start : tuple[int,int], grid : list[list[str]]) -> list[list[int]]:
     height = len(grid)
     width = len(grid[0])
     numbers = [ [ 0 ] * width for _ in range(height) ]
 
-    h = height
-    w = width
-    g = grid 
-    n = numbers
-
-
     step = 0
     sy, sx = start
-
     numbers[sy][sx] = -1
 
-    if g[sy][sx+1] == '─' or g[sy][sx+1] == '┐' or g[sy][sx+1] == '┘':
-        flood(sy, sx+1, step + 1, h, w, g, n)
+    if grid[sy][sx+1] in ['─', '┐', '┘']:
+        flood_pipe(sy, sx+1, step + 1, height, width, grid, numbers)
 
-    if g[sy][sx-1] == '─' or g[sy][sx-1] == '┌' or g[sy][sx-1] == '└':
-        flood(sy, sx-1, step + 1, h, w, g, n)
+    if grid[sy][sx-1] in ['─','┌','└']:
+        flood_pipe(sy, sx-1, step + 1, height, width, grid, numbers)
 
+    if grid[sy+1][sx] in ['│', '┘', '└']:
+        flood_pipe(sy+1, sx, step + 1, height, width, grid, numbers)
 
-    if g[sy+1][sx] == '│' or g[sy+1][sx] == '┘' or g[sy+1][sx] == '└':
-        flood(sy+1, sx, step + 1, h, w, g, n)
-
-    if g[sy-1][sx] == '│' or g[sy-1][sx] == '┌' or g[sy-1][sx] == '┐':
-        flood(sy+1, sx, step + 1, h, w, g, n)
+    if grid[sy-1][sx] in ['│', '┌', '┐']:
+        flood_pipe(sy+1, sx, step + 1, height, width, grid, numbers)
 
     return numbers
 
 
 def part1(filename : str) -> int:
     start, grid = parse_input(filename)
-    numbers = compute_main_loop(start, grid)
-    challenge = find_max(numbers)
+    numbers = compute_main_pipe_loop(start, grid)
+    challenge = find_max_in_grid(numbers)
 
     return challenge
 
@@ -131,10 +128,9 @@ def scale_up(grid : list[list[str]]) -> list[list[str]]:
     height = len(grid)
     width = len(grid[0])
 
-    scaled_grid = []
+    scaled_x_grid = []
     for row in grid:
         scaled_row = []
-
         scaled_row.append('.')
         for x in range(len(row)):
             scaled_row.append(row[x])
@@ -142,21 +138,21 @@ def scale_up(grid : list[list[str]]) -> list[list[str]]:
                 scaled_row.append('─')
             else:
                 scaled_row.append('.')
-        scaled_grid.append(scaled_row)
+        scaled_x_grid.append(scaled_row)
 
-    scaled_grid_2 = []
-    scaled_grid_2.append(['.'] * ((width+1) * 2 - 1) )
-    for y in range(len(scaled_grid)):
+    scaled_xy_grid = []
+    scaled_xy_grid.append(['.'] * ((width+1) * 2 - 1) )
+    for y in range(len(grid)):
         scaled = []
-        for x in range(len(scaled_grid[y])):
-            if scaled_grid[y][x] in ['S', '┌', '│', '┐'] and y+1 < height and scaled_grid[y+1][x] in ['└', '│', '┘']:
+        for x in range(len(scaled_x_grid[y])):
+            if scaled_x_grid[y][x] in ['S', '┌', '│', '┐'] and y+1 < height and scaled_x_grid[y+1][x] in ['└', '│', '┘']:
                 scaled.append('│')
             else:
                 scaled.append('.')
-        scaled_grid_2.append(scaled_grid[y])            
-        scaled_grid_2.append(scaled)
+        scaled_xy_grid.append(scaled_x_grid[y])            
+        scaled_xy_grid.append(scaled)
 
-    return scaled_grid_2
+    return scaled_xy_grid
 
 
 def scale_down(grid : list[list[str]]) -> list[list[str]]:
@@ -173,22 +169,27 @@ def scale_down(grid : list[list[str]]) -> list[list[str]]:
     return scaled_grid
 
 
-
-def flood_grid(y, x, h, w, g):
-    if y < 0 or y >= h or x < 0 or x >= w:
+def flood_grid(y, x, height, width, grid):
+    if  x < 0 or x >= width or y < 0 or y >= height:
         return
 
-    if g[y][x] == '.':
-        g[y][x] = 'O'
+    if grid[y][x] == '.':
+        grid[y][x] = ' ' #'█'
 
-    if x+1 < w and g[y][x+1] == '.':
-        flood_grid(y, x+1, h, w, g)
-    if x > 1 and g[y][x-1] == '.':
-        flood_grid(y, x-1, h, w, g)
-    if y+1 < h and g[y+1][x] == '.':
-        flood_grid(y+1, x, h, w, g)
-    if y > 1 and g[y-1][x] == '.':
-        flood_grid(y-1, x, h, w, g)
+    for nexty, nextx in [(y+1, x), (y-1, x), (y, x+1), (y, x-1)]:
+        if nexty < 0 or nexty >= height or nextx < 0 or nextx >= width:
+            continue
+        if grid[nexty][nextx] == '.':
+            flood_grid(nexty, nextx, height, width, grid)
+
+    # if x+1 < w and g[y][x+1] == '.':
+    #     flood_grid(y, x+1, h, w, g)
+    # if x > 1 and g[y][x-1] == '.':
+    #     flood_grid(y, x-1, h, w, g)
+    # if y+1 < h and g[y+1][x] == '.':
+    #     flood_grid(y+1, x, h, w, g)
+    # if y > 1 and g[y-1][x] == '.':
+    #     flood_grid(y-1, x, h, w, g)
 
 
 def count_char(c : str, grid : list[list[str]]) -> int:
@@ -206,7 +207,9 @@ def keep_main_loop(height, width, grid, numbers):
         for colum in range(width):
             if numbers[row][colum] != 0:
                 new_grid[row][colum] = grid[row][colum]
+
     return new_grid    
+
 
 def part2(filename : str) -> int:
     start, grid = parse_input(filename)
@@ -216,9 +219,9 @@ def part2(filename : str) -> int:
 
     # print_grid(grid)
 
-    numbers = compute_main_loop(start, grid)
+    numbers = compute_main_pipe_loop(start, grid)
     # print_numbers(numbers)
-    
+
     grid = keep_main_loop(height, width, grid, numbers)
     # print_grid(grid)
 
@@ -228,19 +231,18 @@ def part2(filename : str) -> int:
     scaled_height = len(scaled_grid)
     scaled_width = len(scaled_grid[0])
     flood_grid(0, 0, scaled_height, scaled_width, scaled_grid)
-    # print_grid(scaled_grid)
+    print_grid(scaled_grid)
 
     scaled_down_grid = scale_down(scaled_grid)
     # print_grid(scaled_down_grid)
 
     challenge = count_char('.', scaled_down_grid)
 
-
     return challenge
 
 
 import sys
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(20000)
 
 assert part1('./sample.txt') == 4
 assert part1('./sample2.txt') == 8
