1de53d7a42f677c4220142281b08aa7be07d5b1a
Gerald Oster <gerald.oster@loria.fr>
2023-12-15 07:30:12 +0100

    [day15] python solution -- didn't know for...else...

--- aoc2023/day15/day15.py
+++ aoc2023/day15/day15.py
@@ -1,9 +1,9 @@
 def read_input(filename: str) -> list[str]:
     data = []
     with open(filename, "r") as input_file:
-        data = input_file.readline().rstrip().split(',')
-
+        data = input_file.readline().rstrip().split(",")
     return data
+
 
 def die(msg: str):
     raise Exception(msg)
@@ -20,60 +20,49 @@ def hash(str) -> int:
 
 def part1(filename: str) -> int:
     data = read_input(filename)
-    return sum(( hash(ins) for ins in data ))
+    return sum((hash(ins) for ins in data))
 
 
 def part2(filename: str) -> int:
     data = read_input(filename)
-    boxes = [ list() for _ in range(256) ]
+    boxes = [list() for _ in range(256)]
 
     for instruction in data:
-        if instruction[-1] == '-' in instruction:
-            op = '-'
+        if instruction[-1] == "-":
             label = instruction[:-1]
+            box_id = hash(label)
 
-            k = hash(label)
-            for idx, lens in enumerate(boxes[k]):
+            box = boxes[box_id]
+            for idx, lens in enumerate(box):
                 if lens[0] == label:
-                    boxes[k].pop(idx)
+                    box.pop(idx)
                     break
         else:
-            op = '='
-            label, focal_length = instruction.split('=')
+            label, focal_length = instruction.split("=")
             focal_length = int(focal_length)
 
-            k = hash(label)
-            if not boxes[k]:
-                # if empty
-                boxes[k].append((label, focal_length))
-            else:
-                done = False
-                for idx, lens in enumerate(boxes[k]):
-                    if lens[0] == label:
-                        boxes[k][idx] = (label, focal_length)
-                        done = True
-                        break
-                if not done:
-                    boxes[k].append((label, focal_length))
+            box_id = hash(label)
 
-    # print boxes
-    #
-    # for i in range(0,4):
-    #     print(f'Box {i:3}: ', list(boxes[i]))
+            box = boxes[box_id]
+            for idx, lens in enumerate(box):
+                if lens[0] == label:
+                    box[idx] = (label, focal_length)
+                    break
+            else:
+                box.append((label, focal_length))
 
     focusing_power = 0
     for idx_box, box in enumerate(boxes):
         for idx_lens, lens in enumerate(box):
-            focusing_power += (idx_box + 1) * (idx_lens + 1)  * lens[1]
+            focusing_power += (idx_box + 1) * (idx_lens + 1) * lens[1]
 
     return focusing_power
 
 
-assert hash('HASH') == 52
+assert hash("HASH") == 52
 
 assert part1("./sample.txt") == 1320
 assert part1("./input.txt") == 513172
 
 assert part2("./sample.txt") == 145
 assert part2("./input.txt") == 237806
-
