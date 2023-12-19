27c7cba3e4049e1a38dfdd3ca41652ee5dbe4d65
Gerald Oster <gerald.oster@loria.fr>
2023-12-19 18:40:50 +0100

    [day19] python solution (cleanup)

--- aoc2023/day19/day19.py
+++ aoc2023/day19/day19.py
@@ -1,8 +1,5 @@
-import sys
-from typing import Any
+from typing import Any, Callable, Iterator
 from functools import partial
-
-# sys.setrecursionlimit(100000)
 
 
 def die(msg: str):
@@ -10,184 +7,138 @@ def die(msg: str):
     raise Exception(msg)
 
 
-def apply(workflows : dict[str, Any], workflow_name: str, value : Any) -> Any:
-    return workflows[workflow_name](value=value)
-
-def inf_part1(workflows : dict[str, Any], var: str, cond: int, f: Any, g: Any, value : Any) -> Any:
-    if value[var] < cond:
-        f(value=value)
-    else:
-        g(value=value)
-
-def sup_part1(workflows : dict[str, Any], var: str, cond: int, f: Any, g: Any, value : Any) -> Any:
-    if value[var] > cond:
-        f(value=value)
-    else:
-        g(value=value)
-    
-
-
-sum_part_1 = 0
-def accept_part1(workflows : dict[str, Any], value : Any) -> Any:
-    global sum_part_1
-    sum_part_1 += value['x'] + value['m'] + value['a'] + value['s']
-
-
-def reject(workflows : dict[str, Any], value : Any) -> Any:
-    return 
-
-
-def parse_line(line: str, workflows : dict[str, Any], accept_function, inf_function, sup_function) -> tuple[str, Any]:
+def parse_task(
+    line: str, tasks: dict[str, Callable], accept: Callable, reject: Callable, inf: Callable, sup: Callable
+) -> tuple[str, Any]:
     name, rest = line.rstrip().rstrip("}").split("{")
-
-    function = None
-
-    reject_function = partial(reject, workflows=workflows)
-
-    last_op = None
-
+    last_function = None
     for op in reversed(rest.rstrip("}").split(",")):
-        match op:
-            case 'A':
-                function = partial(accept_function, workflows=workflows)
-            case 'R':
-                function = partial(reject, workflows=workflows)
-            case _:
-                if '>' in op:
-                    var, rest = op.split('>')
-                    cond, workflow_name = rest.split(':')
-                    if workflow_name == 'A':
-                        f=accept_function
-                    elif workflow_name == 'R':
-                        f=reject_function
-                    else:
-                        f=partial(apply, workflows=workflows, workflow_name=workflow_name)
-                    function = partial(sup_function, workflows=workflows, var=var, cond=int(cond), f=f, g=last_op)
-                elif '<' in op:
-                    var, rest = op.split('<')
-                    cond, workflow_name = rest.split(':')
-                    if workflow_name == 'A':
-                        f=accept_function
-                    elif workflow_name == 'R':
-                        f=reject_function
-                    else:
-                        f=partial(apply, workflows=workflows, workflow_name=workflow_name)
-                    function = partial(inf_function, workflows=workflows, var=var, cond=int(cond), f=f, g=last_op)
-                else:
-                    workflow_name = op
-                    function = partial(apply, workflows=workflows, workflow_name=workflow_name)
-        last_op = function
+        function = None
+        if op == "A":
+            function = accept
+        elif op == "R":
+            function = reject
+        elif ">" in op:
+            var, rest = op.split(">")
+            cond, task_name = rest.split(":")
+            if task_name == "A":
+                f = accept
+            elif task_name == "R":
+                f = reject
+            else:
+                f = partial(apply, tasks=tasks, task_name=task_name)
+            function = partial(sup, var=var, cond=int(cond), f=f, g=last_function)
+        elif "<" in op:
+            var, rest = op.split("<")
+            cond, task_name = rest.split(":")
+            if task_name == "A":
+                f = accept
+            elif task_name == "R":
+                f = reject
+            else:
+                f = partial(apply, tasks=tasks, task_name=task_name)
+            function = partial(inf, var=var, cond=int(cond), f=f, g=last_function)
+        else:
+            task_name = op
+            function = partial(apply, tasks=tasks, task_name=task_name)
+        last_function = function
 
-    return name, last_op
+    return name, last_function
 
 
-def read_input_part1(filename: str) -> tuple[dict[str,Any], list[int]]:
-    workflows = {}
+def parse_tasks(lines: Iterator[str], tasks: dict[str, Callable], accept: Callable, reject: Callable, inf: Callable, sup: Callable):
+    line = next(lines).rstrip()
+    while line != "\n":
+        task_name, task_function = parse_task(line, tasks, accept, reject, inf, sup)
+        tasks[task_name] = task_function
+        line = next(lines)
+
+
+def parse_values(lines: Iterator[str]) -> list[Any]:
     values = []
+    for line in lines:
+        line = line.rstrip()
 
-    accept_function = partial(accept_part1, workflows=workflows)
+        x, m, a, s = line.rstrip()[1:-1].split(",")
+        value = {"x": int(x[2:]), "m": int(m[2:]), "a": int(a[2:]), "s": int(s[2:])}
+        values.append(value)
+    return values
 
 
+def reject(value: Any) -> int:
+    return 0
 
-    with open(filename, "r") as input_file:
-        lines = input_file.readlines().__iter__()
 
-        line = next(lines).rstrip()
-        while line != '\n':
-            workflow_name, workflow_function = parse_line(line, workflows, accept_function, inf_part1, sup_part1)
-            workflows[workflow_name] = workflow_function
-            line = next(lines)
-
-        for line in lines:
-            line = line.rstrip()
-
-            x, m, a, s = line.rstrip()[1:-1].split(",")
-            value = { 'x': int(x[2:]), 'm': int(m[2:]), 'a': int(a[2:]), 's': int(s[2:]) }
-            values.append(value)
-
-    return workflows, values
+def apply(tasks: dict[str, Callable], task_name: str, value: Any) -> int:
+    return tasks[task_name](value=value)
 
 
 def part1(filename: str) -> int:
-    global sum_part_1
-    sum_part_1 = 0
-
-    workflows, values = read_input_part1(filename)
-
-    for value in values:
-        workflows['in'](value=value)
-
-    return sum_part_1
-
-
-# assert part1("./sample.txt") == 19114
-# assert part1("./input.txt") == 342650
-
-
-sum_part_2 = 0
-def accept_part2(workflows : dict[str, Any], value : Any) -> Any:
-    global sum_part_2
-    sum_part_2 += (value['x'][1]-value['x'][0]+1) * (value['m'][1]-value['m'][0]+1) * (value['a'][1]-value['a'][0]+1) * (value['s'][1]-value['s'][0]+1)
-
-def inf_part2(workflows : dict[str, Any], var: str, cond: int, f: Any, g: Any, value : Any) -> Any:
-    min, max = value[var]
-
-    #if value[var] < cond:
-    v = value.copy()
-    v[var] = (min, cond-1)
-    f(value=v)
-    #else:
-    v = value.copy()
-    v[var] = (cond, max)
-    g(value=v)
-
-def sup_part2(workflows : dict[str, Any], var: str, cond: int, f: Any, g: Any, value : Any) -> Any:
-    min, max = value[var]
-    # if value[var] > cond:
-    v = value.copy()
-    v[var] = (cond+1, max)
-    f(value=v)
-    # else:
-    v = value.copy()
-    v[var] = (min, cond)
-    g(value=v)
-    
-
-
-def read_input_part2(filename: str) -> dict[str, Any]:
-    workflows = {}
+    tasks = {}
     values = []
 
-    accept2 = partial(accept_part2, workflows=workflows)
-    inf2 = partial(inf_part2, workflows=workflows)
-    sup2 = partial(sup_part2, workflows=workflows)
+    def inf_part1(var: str, cond: int, f: Callable, g: Callable, value: Any) -> int:
+        if value[var] < cond:
+            return f(value=value)
+        else:
+            return g(value=value)
 
+    def sup_part1(var: str, cond: int, f: Callable, g: Callable, value: Any) -> int:
+        if value[var] > cond:
+            return f(value=value)
+        else:
+            return g(value=value)
+
+    def accept_part1(value: Any) -> int:
+        return value["x"] + value["m"] + value["a"] + value["s"]
 
     with open(filename, "r") as input_file:
-        lines = input_file.readlines().__iter__()
+        parse_tasks(input_file, tasks, accept_part1, reject, inf_part1, sup_part1)
+        values = parse_values(input_file)
 
-        line = next(lines).rstrip()
-        while line != '\n':
-            workflow_name, workflow_function = parse_line(line, workflows, accept2, inf2, sup2)
-            workflows[workflow_name] = workflow_function
-            line = next(lines)
+    return sum(tasks["in"](value=value) for value in values)
 
-    return workflows
+
+assert part1("./sample.txt") == 19114
+assert part1("./input.txt") == 342650
 
 
 def part2(filename: str) -> int:
-    global sum_part_2
-    sum_part_2 = 0
+    def accept_part2(value: Any) -> int:
+        return (
+            (value["x"][1] - value["x"][0] + 1)
+            * (value["m"][1] - value["m"][0] + 1)
+            * (value["a"][1] - value["a"][0] + 1)
+            * (value["s"][1] - value["s"][0] + 1)
+        )
 
-    workflows = read_input_part2(filename)
+    def inf_part2(var: str, cond: int, f: Callable, g: Callable, value: Any) -> int:
+        min, max = value[var]
+        v = value.copy()
+        v[var] = (min, cond - 1)
+        left = f(value=v)
+        v[var] = (cond, max)
+        right = g(value=v)
+        return left + right
 
-    workflows['in'](value={'x': (1, 4000), 'm': (1, 4000), 'a': (1, 4000), 's': (1, 4000)})
+    def sup_part2(var: str, cond: int, f: Callable, g: Callable, value: Any) -> int:
+        min, max = value[var]
+        v = value.copy()
+        v[var] = (cond + 1, max)
+        left = f(value=v)
+        v[var] = (min, cond)
+        right = g(value=v)
+        return left + right
 
-    return sum_part_2
+    tasks = {}
+    with open(filename, "r") as input_file:
+        parse_tasks(input_file, tasks, accept_part2, reject, inf_part2, sup_part2)
+
+    interval = (1, 4000)
+    initial_value = {"x": interval, "m": interval, "a": interval, "s": interval}
+
+    return tasks["in"](value=initial_value)
 
 
 assert part2("./sample.txt") == 167409079868000
 assert part2("./input.txt") == 130303473508222
-
-
-
