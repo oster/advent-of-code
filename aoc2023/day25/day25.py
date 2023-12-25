05b2b882ad2c7c8004a867c75931ed76db573a7c
Gerald Oster <gerald.oster@loria.fr>
2023-12-25 20:08:24 +0100

    [day25] python solution (randomized algorithm)

--- aoc2023/day25/day25.py
+++ aoc2023/day25/day25.py
@@ -1,3 +1,7 @@
+from collections import Counter, deque
+import random
+
+
 def die(msg: str):
     raise Exception(msg)
 
@@ -71,3 +75,67 @@ def part1(filename: str) -> int:
 
 assert part1("./sample.txt") == 54
 assert part1("./input.txt") == 562772
+
+
+def window(iterable):
+    return zip(iterable, iterable[1:])
+
+
+def build_path_back(src, dst, from_to):
+    # reconstruct path
+    path = []
+    c = src
+    while c != dst:
+        path.append(c)
+        c = from_to[c][0]
+    return list(reversed(path))
+
+
+def find_shortest_path(graph, start, end):
+    distances = {start: (start, 0)}
+    q = deque()
+    q.append((start, 0))
+
+    while q:
+        at, dist = q.popleft()
+        for next in graph[at]:
+            if next not in distances or distances[next][1] > dist + 1:
+                distances[next] = (at, dist + 1)
+                q.append((next, dist + 1))
+    return build_path_back(end, start, distances)
+
+
+def part1_alternate(filename: str) -> int:
+    graph = read_input(filename)
+
+    some_node_in_cut1 = None
+
+    for _ in range(3):
+        count = Counter()
+        for _ in range(50):
+            nodes = list(graph.keys())
+
+            nodeA = nodes[random.randrange(0, len(nodes))]
+            nodeB = nodes[random.randrange(0, len(nodes))]
+            if nodeA != nodeB:
+                p = find_shortest_path(graph, nodeA, nodeB)
+                for src, dst in window(p):
+                    count.update([(src, dst)])
+
+        to_remove = [edge for edge, _ in count.most_common(1)]
+
+        if not some_node_in_cut1:
+            some_node_in_cut1 = to_remove[0][0]
+
+        for node, other_node in to_remove:
+            graph[node].remove(other_node)
+            graph[other_node].remove(node)
+
+    a = len(collect_nodes_dfs(graph, some_node_in_cut1, set()))
+    b = len(graph) - a
+
+    return a * b
+
+
+assert part1_alternate("./sample.txt") == 54
+assert part1_alternate("./input.txt") == 562772
