5a1995703175335380595244bcd7a1b9b5400a12
Gerald Oster <gerald.oster@loria.fr>
2024-12-19 21:15:01 +0100

    [day19-aoc2024] python solution (clean)

--- aoc2024/day19/day19.py
+++ aoc2024/day19/day19.py
@@ -4,51 +4,56 @@ from icecream import ic
 def read_data(filename: str) -> tuple[list[str], list[str]]:
     with open(filename, "r") as data_file:
         t, ds = data_file.read().split("\n\n")
-        towels = list(map(str.strip, t.split(",")))
-        designs = [d.strip() for d in ds.split("\n")][:-1]
+        towels = t.split(", ")
+        designs = [d.strip() for d in ds.rstrip().split("\n")]
     return towels, designs
 
 
-def can_build(depth: int, design: str, towels: list[str]) -> bool:
+def can_build(design: str, towels: list[str]) -> bool:
     if len(design) == 0:
         return True
+
+    if design in mem:
+        return mem[design]
 
     for towel in towels:
         l = len(towel)
         if design[:l] == towel:
-            ok = can_build(depth + l, design[l:], towels)
+            ok = can_build(design[l:], towels)
             if ok:
+                mem[design] = True
                 return True
 
+    mem[design] = False
     return False
 
 
 def part1(filename: str) -> int:
+    global mem
+    mem.clear()
+
     towels, designs = read_data(filename)
     towels = sorted(towels, key=lambda x: len(x), reverse=True)
 
-    count = 0
-    for d in designs:
-        if can_build(0, d, towels):
-            count += 1
+    count = sum(can_build(d, towels) for d in designs)
     return count
 
 
 mem = {}
 
-def can_build_all(design: str, towels: list[str]) -> int:
-    if design in mem:
-        return mem[design]
 
+def can_build_all(design: str, towels: list[str]) -> int:
     if len(design) == 0:
         return 1
+
+    if design in mem:
+        return mem[design]
 
     res = 0
     for towel in towels:
         l = len(towel)
         if design[:l] == towel:
-            ok = can_build_all(design[l:], towels)
-            res += ok
+            res += can_build_all(design[l:], towels)
 
     mem[design] = res
     return res
@@ -56,13 +61,12 @@ def can_build_all(design: str, towels: list[str]) -> int:
 
 def part2(filename: str) -> int:
     global mem
-    mem = {}
+    mem.clear()
+
     towels, designs = read_data(filename)
     towels = sorted(towels, key=lambda x: len(x), reverse=True)
-    count = 0
-    for d in designs:
-        r = can_build_all(d, towels)
-        count += r
+
+    count = sum(can_build_all(d, towels) for d in designs)
     return count
 
 
